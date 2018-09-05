46c7e59fa45a4eb1428094af0ec4f4a50f75b5ef
neerajibs <37495935+neerajibs@users.noreply.github.com>
2018-09-05 09:07:19 +0530

    Update beyond-compare.sh

--- Ubuntu-16.04/beyond-compare.sh
+++ Ubuntu-16.04/beyond-compare.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ########################################################
 # Written to be used on 64 bits computers
-# Written By    :       Vinod Kumar
 ########################################################
 # Downloading & Installing Beyond Compare for Ubuntu Version
 sudo wget https://www.scootersoftware.com/bcompare-4.2.3.22587_amd64.deb -O /tmp/bcompare-4.2.3.22587_amd64.deb
