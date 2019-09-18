dcd653ef208fb8f3a9b4b031f9698e43208c6175
Your Name <you@example.com>
2019-09-18 14:16:59 +0530

    updated maven

---
+++ Ubuntu-16.04/maven-3.6.0.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+#######################################################
+# Installing All required Package for Ubuntu Version
+
+
+sudo cd /usr/local
+wget https://www-eu.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
+sudo tar xzf apache-maven-3.6.0-bin.tar.gz
+sudo ln -s apache-maven-3.6.0 apache-maven
+sudo apt-get update -y
+
+
+echo "export JAVA_HOME=/usr/lib/jvm/java-11-oracle
+export M2_HOME=/usr/local/apache-maven
+export MAVEN_HOME=/usr/local/apache-maven
+export PATH=${M2_HOME}/bin:${PATH} " > /etc/profile.d/apache-maven.sh
+
+Yogesh
+
+
+
+#sudo apt-get install maven -y
+
+
+
+echo "################ Maven Installation Sucessfully ######################"
+
