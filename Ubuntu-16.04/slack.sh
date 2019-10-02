e1c3a529f47208ec83bdfae5902d71112cd938b0
neeraj.joshi <neeraj.joshi@xyz.com>
2019-10-02 09:55:57 -0700

    slack version updated

--- Ubuntu-16.04/slack.sh
+++ Ubuntu-16.04/slack.sh
@@ -4,9 +4,9 @@
 # Written to be used on 64 bits computers
 # Written By 	: 	Vinod Kumar
 ###############################################
-rm /tmp/slack-desktop-2.8.2-amd64.deb
-sudo wget http://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.2-amd64.deb -O /tmp/slack-desktop-2.8.2-amd64.deb
-sudo dpkg -i /tmp/slack-desktop-2.8.2-amd64.deb
+rm /tmp/slack-desktop-4.0.2-amd64.deb
+sudo wget http://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb -O /tmp/slack-desktop-4.0.2-amd64.deb
+sudo dpkg -i /tmp/slack-desktop-4.0.2-amd64.deb
 echo "fixing hardcoded icon"
 old="Icon=\/usr\/share\/pixmaps\/slack.png"
 new="Icon=slack"
