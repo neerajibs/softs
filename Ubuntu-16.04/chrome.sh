b93e600027a5e77ed83517bf31ff1cfbad0460e3
neerajibs <37495935+neerajibs@users.noreply.github.com>
2020-02-02 00:00:51 +0530

    Update chrome.sh

--- Ubuntu-16.04/chrome.sh
+++ Ubuntu-16.04/chrome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ########################################################
 # Written to be used on 64 bits computers
-# Written By    :       Vinod Kumar
+# Written By    :       Neeraj Joshi
 ######################################################
 # Downloading & Installing Chrome for Ubuntu Version
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
