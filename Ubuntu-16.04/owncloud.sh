183feedab09238efae05d6377a17bf67c242c87e
neerajibs <neeri92@gmail.com>
2022-01-16 10:01:37 +0000

    test

--- Ubuntu-16.04/owncloud.sh
+++ Ubuntu-16.04/owncloud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ########################################################
 # Written to be used on 64 bits computers
-# Written By    :       Vinod Kumar
+# Written By    :       Neeraj Joshi
 ########################################################
 # Downloading & Installing Owncloud Client for Ubuntu Version
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_15.04/ /' &gt;&gt; /etc/apt/sources.list.d/owncloud-client.list" 
